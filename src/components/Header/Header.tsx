ebd150d2d5f98d4dc79ab30962c2fd1657f03959
unknown <alekseytanaga@yandex.ru>
2025-01-18 12:03:54 +0300

    setup and starting the project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+export const Header = () => {
+  return (
+    <div>
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+    </div>
+  )
+}
+
+
